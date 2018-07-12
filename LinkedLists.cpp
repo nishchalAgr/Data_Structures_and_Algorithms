62ccd5407ea8caf37713640450a383985727a39f
nishchalagrawal <nishchalagrawal123@gmail.com>
2018-07-11 18:54:28 -0700

    Added c++ implementation of stacks for linked lists

--- LinkedLists.cpp
+++ LinkedLists.cpp
@@ -3,12 +3,119 @@
 
 using namespace std;
 
-void createnode(int value);
+int const MAX = 10;
 
 struct node{
 
 	int data;
 	node *next;
+	node *back;
+
+};
+
+class stacks{
+	//in here, the top is equal to the tail
+	private:
+
+		node *head, *top;
+
+	public:
+	stacks(){
+		
+		head = NULL;
+		top = NULL;
+
+	}
+
+	int numOfElements(){
+
+		node *temp = new node;
+
+		temp = head;
+
+		int i = 0;
+
+		while(temp != NULL){
+
+			i++;
+			temp = temp->next;
+
+		}
+
+		return i;
+
+	}
+
+	void push(int value){
+
+		if(numOfElements() == MAX){
+
+			cout << "Stack Overflow" << endl;
+
+		}
+
+		node *temp = new node;
+		node *previous = new node;
+
+		temp->data = value;
+		temp->next = NULL;
+		temp->back = NULL;
+
+		if(top == NULL){
+
+			head = temp;
+			top = temp;
+			temp = NULL;
+
+		}else{
+
+			previous = head;
+			top->next = temp;
+			top = temp;
+
+			while(previous->next != top){
+
+				previous = previous->next;
+
+			}
+
+			top->back = previous;
+
+		}
+
+	}
+
+	void pop(){
+
+		if(top == NULL){
+
+			cout << "Stack Underflow" << endl;
+			return;
+
+		}else{
+
+			top = top->back;
+
+		}
+
+	}
+
+	void display(){
+
+		node *temp = new node;
+		
+		temp = top;
+
+		while(temp != NULL){
+			//prints out the value temp is pointing to, and then makes temp point to the next node
+			cout << temp->data << endl;
+			temp = temp->back;
+
+		}
+
+		cout << endl;
+
+	}
 
 };
 
@@ -49,6 +156,8 @@ class list{
 	}
 
 	void display(){
+
+		cout << "head is :" << head->data << endl;
 
 		node *temp = new node;
 		
@@ -164,18 +273,20 @@ class list{
 
 int main(){
 
-	list arr;
+	stacks arr;
 
-	arr.createnode(10);
-	arr.createnode(14);
-	arr.createnode(2);
-	arr.createnode(7);
-	arr.createnode(3);
-	arr.createnode(8);
+	arr.push(3);
+	arr.push(7);
+	arr.push(1);
+	arr.push(8);
+	arr.push(0);
+	arr.push(2);
 
 	arr.display();
 
-	arr.linearSearch(8);
+	arr.pop();
+
+	arr.display();
 
 	return 0;
 
